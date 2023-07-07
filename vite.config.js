d6b873818d6d31d66fdf4fe82a86768781c5b196
minjiBaek <emilyback@naver.com>
2023-07-07 14:47:10 +0900

    main setting

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import vitePluginGLSL from 'vite-plugin-glsl';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vitePluginGLSL()],
+  base: ''
+})

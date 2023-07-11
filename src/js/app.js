317665ad1c8c3eb3955c2a1243126487f8ea139b
minjiBaek <emilyback@naver.com>
2023-07-11 23:19:28 +0900

    planet에 text 추가 및 hovet 시 표출 event 추가, click시 표출 animaiton 유지

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,6 @@ import Planet from './models/Planet';
 import GUI from 'lil-gui';
 import Data from './data/data';
 import {FontLoader} from 'three/examples/jsm/loaders/FontLoader';
-import {TextGeometry} from 'three/examples/jsm/geometries/TextGeometry';
 import gsap from 'gsap';
 import {ScrollTrigger} from 'gsap/ScrollTrigger';
 
@@ -17,6 +16,15 @@ export default async function init () {
   const container = document.querySelector('#app');
   gsap.registerPlugin(ScrollTrigger); // scrollTrigger을 사용할수있음
   const canvas = document.querySelector('#canvas');
+
+  // const loadingManger = new THREE.LoadingManager();
+  // loadingManger.onProgress = (url, loaded, total) => {
+  //   //총량: total, 진행상태: loaded
+  //   bar.value = (loaded / total) * 100;
+  // }
+  // loadingManger.onLoad = () => {
+  //   barContainer.style.display = 'none';
+  // }
 
   const renderer = new THREE.WebGLRenderer({
     antialias: true, //pixel 다듬기?
@@ -32,6 +40,7 @@ export default async function init () {
   };
   const clock = new THREE.Clock();
   const projectTextureLoader = new THREE.TextureLoader();
+  // const fontLoader = new FontLoader(loadingManger);
   const fontLoader = new FontLoader();
 
   projectTextureLoader.setPath('../../assets/projects/');
@@ -44,13 +53,14 @@ export default async function init () {
     100
   );
  
-  // -0.073, -0.017, 3.689
   camera.position.set( -0.073, -0.017, 3.689);
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true;
   controls.maxAzimuthAngle = Math.PI / 2;
   controls.maxPolarAngle = Math.PI ;
   controls.enableZoom = false;
+  controls.enablePan = false;
+
   controls.dampingFactor = 0.1;
   controls.saveState();
  
@@ -58,37 +68,16 @@ export default async function init () {
   const pointer = new THREE.Vector2();
   const data = new Data({about:'carrer'});
   const font =  await fontLoader.loadAsync('../../assets/fonts/Gugi_Regular.json');
-  let numberText = null;
-  let titleText = null;
-  let scrollCont = 0;
+
   let changeObjArry = [];
   let vecArry = [];
   const clickAni = gsap.timeline({
     id: 'click',
     smoothChildTiming: true,
     autoRemoveChildren: true,
-
   });
 
-  const createText = (content) => {
-    const textGeometry = new TextGeometry(content,{ 
-      font : font,
-      size: 0.5,
-      height: 0.1,
-      bevelEnabled: true,
-      bevelSegments: 5,
-      bevelSize: 0.01,
-      bevelThickness: 0.02
-    });
-    textGeometry.computeBoundingBox(); //boundingbox의 값 계산하는 함수, 그 전엔 값 안나옴
-    textGeometry.center();
-    const textMeterial = new THREE.MeshLambertMaterial({
-      color: 0x33ffdd,
-      emissive: 0xccff77,
-    });
-    const text = new THREE.Mesh(textGeometry, textMeterial);
-    return text;
-  }
+
 
   const create = (gui) => {
     // const earth = new Earth({ baseR: 0.8, glowR: 1, texture: textureLoader.load('../../assets/2k_earth_specular_map.png')});
@@ -96,156 +85,23 @@ export default async function init () {
     const planetRadius = 3;
     let plantArry = [];
     for (let i = 60; i <= 360; i += 60) {
-      const planetG = new Planet({cubeR: 0.22, skeleR: 0.4, texture: projectTextureLoader.load(data.data[i/60 - 1].img), index: i, planetRadius: planetRadius });
+      const planetG = new Planet({cubeR: 0.22, skeleR: 0.4, texture: projectTextureLoader.load(data.data[i/60 - 1].img), index: i, planetRadius: planetRadius, font: font, data: data });
       planetGroup.name = 'renderPlanet';
       planetGroup.add( planetG.planet);
       plantArry.push(planetG);
       
-      //font test
-
-  /*
-      // if(i/60 === 4){
-        
-      //   const text = gui.addFolder('titleText').close();
-         
-      //   text.add(titleText.rotation, 'x')
-      //   .min(-10)
-      //   .max(10)
-      //   .step(0.001);
-    
-      //   text
-      //   .add(titleText.rotation, 'y')
-      //   .min(-10)
-      //   .max(10)
-      //   .step(0.001);
-    
-    
-      //   text
-      //   .add(titleText.rotation, 'z')
-      //   .min(-10)
-      //   .max(10)
-      //   .step(0.001);
-
-      //   const textrotat = gui.addFolder('titleText').close();
-         
-      //   textrotat.add(titleText.position, 'x')
-      //   .min(-10)
-      //   .max(10)
-      //   .step(0.001);
-    
-      //   textrotat
-      //   .add(titleText.position, 'y')
-      //   .min(-10)
-      //   .max(10)
-      //   .step(0.001);
-    
-    
-      //   textrotat
-      //   .add(titleText.position, 'z')
-      //   .min(-10)
-      //   .max(10)
-      //   .step(0.001);
-      // }
-    */
-     
-    
     };
     planetGroup.rotation.set(4.733, 2.702, 0.49 );
 
-
-    //font 추후 추가
-    /*
-    // scene.add(earth.earth, planetGroup );
-    // numberText = createText('1');
-    // titleText = createText(data.data[3].title);
-    // numberText.position.set(planetGroup.children[3].position.x, planetGroup.children[3].position.y, planetGroup.children[3].position.z);
-    // numberText.rotation.set(-1.832, 4.23, -0.273);
-    // numberText.position.set(-1.026, -2.895, -0.504);
- 
-    // numberText.rotation.set(-1.832, 4.23, -0.273);
-    // titleText.rotation.set(-1.468, 3.582, -0.299);
-    // numberText.visible = false;
-    // const text = gui.addFolder('titleText').close();
-    //    text.add(numberText.rotation, 'x')
-    //     .min(-10)
-    //     .max(10)
-    //     .step(0.001);
-    
-    //     text
-    //     .add(numberText.rotation, 'y')
-    //     .min(-10)
-    //     .max(10)
-    //     .step(0.001);
-    
-    
-    //     text
-    //     .add(numberText.rotation, 'z')
-    //     .min(-10)
-    //     .max(10)
-    //     .step(0.001);
-
-    //     const textrotat = gui.addFolder('titleText').close();
-         
-    //     textrotat.add(numberText.position, 'x')
-    //     .min(-10)
-    //     .max(10)
-    //     .step(0.001);
-    
-    //     textrotat
-    //     .add(numberText.position, 'y')
-    //     .min(-10)
-    //     .max(10)
-    //     .step(0.001);
-    
-    
-    //     textrotat
-    //     .add(numberText.position, 'z')
-    //     .min(-10)
-    //     .max(10)
-    //     .step(0.001);
-    // titleText.visible = false;
-
-    
-          // gsap.fromTo(numberText.rotation, {x: 0, y: 0, z: 0 }, {
-          //   x: -1.832, y:  4.23, z:-0.299, duration: 1,
-          //   // onUpdate: () => {
-          //   //   numberText.rotation.x = clock.getElapsedTime * 2;
-          //   //   numberText.rotation.y = clock.getElapsedTime * 2;
-          //   // }
-          // })
-          // gsap.fromTo(numberText, {opacity: 0, scale: new THREE.Vector3(0,0,0), position: object.position }, {
-          //   visible : true,
-          //   opacity: 0,
-          //   scale: new THREE.Vector3(1,1,1),
-          //   position: new THREE.Vector3(-1.026, -2.895, -0.504),
-          //   duration: 1,
-          // })
-          // gsap.fromTo(numberText,{
-          //   visible : 1,
-          //   y : 1,
-          //   z : 1,
-          //   duration: 1,
-          //   onStart: ()=> {
-          //     numberText.visible = true;
-          //     titleText.visible = true;
-          //     numberText.position.set(-1.026, -2.895, -0.504);
-          //     titleText.position.set(-1.522, -2.853, 0.252);
-          //     titleText.scale.set(0.3, 0.3, 0.3);
-          //   }
-          // });
-          */
-
     scene.add(planetGroup);
-    // scene.add(planetGroup, numberText, titleText);
    
-    // console.log( camera.position.dot(plantArry[3].planet.position))
-    // controls.target.distanceTo(controls.object.position)
     const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
     directionalLight.castShadow = true;
     scene.add( directionalLight);
 
     vecArry = plantArry.flatMap((arr, index)=>{
       const div = document.createElement('div');
+      // div.innerText = 'Please Double-Click If you want see more info about it'
       div.className = 'scroll-planet';
       div.id = `planet-${index}`;
       container.appendChild(div);
@@ -288,15 +144,17 @@ export default async function init () {
             return;
           }
           //같은게 없어서 array에 넣음
-         
-          // console.log(selectArr[0]);
           changeObjArry.push(object);// 커지는거 
         }
 
     }else if(changeObjArry.length > 0 ){
       // console.log("나갈때 여기 아니냐",changeObjArry )//작아지는거
       changeObjArry = [];
-      plantArry.forEach(arr=>{arr.animation('small')}) //animation
+      plantArry.forEach( arr=>{
+        if(!arr.isClick){
+          arr.animation('small');
+        }
+      }) //animation
       
     }else{
       // console.log('비었답니다')
@@ -304,57 +162,31 @@ export default async function init () {
   }
 
   function handlerPointerDown (event, plantArry) {
-    // pointer.x = (event.clientX / window.innerWidth -0.5) * 2; // 중심이 0,0이고 1~-1 사이인 x인 pointer
-    // pointer.y = -(event.clientY / window.innerHeight -0.5) * 2;// 중심이 0,0이고 1~-1 사이인 y인 pointer
-    
-    // raycatster.setFromCamera(pointer, camera);
+
     const intersects = raycatster.intersectObjects(scene.children);
-    console.log(vecArry)
+
     if(intersects.length > 0  && !clickAni.isActive()){
       const object = intersects[0].object;
         if(object.name === 'skeletone' || object.name === 'cube'){
           controls.reset();
           let repeatCount = (object.userData.index -3);
-          
           if(repeatCount < 0){
-            repeatCount = repeatCount + 6;
+            repeatCount = repeatCount + plantArry.length;
           }
-          console.log(repeatCount);
-          // gsap.getById(`#${container.children[repeatCount].id}`).scrollTrigger.scroll(`#${container.children[repeatCount].id}`);
           // 현재 보이는 index랑 click한 index와의 차이 == i
-         
+          const selectArr = plantArry.filter((arry) => {return arry.planet.id === object.parent.id} );
+          if(selectArr[0].isClick){
+            return;
+          }
+          plantArry.forEach( arr=>{ arr.animation('small'); arr.isClick = false;}) //animation
+          selectArr[0].isClick = true;
+          document.querySelector(`#${container.children[repeatCount].id}`).scrollIntoView( {behavior:'smooth'})
+          selectArr[0].animation('big');
+          document.querySelector('#warning-click').setAttribute('class','show');
         }
-        
-        
     }else{
-     
+      reset(plantArry);
     }  
-  }
-
-  const textUpdate = ()=> {
-    if(changeObjArry > 0){
-      gsap.to(numberText.position, { x: -1.026, y: -2.895, z:-0.504,
-        onStart: ()=> {
-          numberText.visible = true
-        },} );
-    }else{
-
-      gsap.fromTo(numberText.scale, {x: 0, y: 0, z: 0 }, {
-        x: 1, y: 1, z:1, duration: 1,
-        onStart: ()=> {
-          numberText.visible = true
-        },
-      })
-      gsap.fromTo(numberText.position, {x: object.position.x, y: object.position.y, z: object.position.y }, {
-        x: -1.026, y: -2.895, z:-0.504, duration: 1,
-        onStart: ()=>{
-          numberText.position.set(  -1.026,  -2.895,-0.504);
-        }
-      })
-    }
-
-    gsap.to(numberText.position, {x: obj.position.x, y: obj.position.y, z: obj.position.y, duration: 1 }, 
-      )
   }
 
 
@@ -363,13 +195,14 @@ export default async function init () {
       const scrollTimeline = gsap.timeline({
         id: `#${container.children[i].id}`,
         scrollTrigger: {
-        trigger: `#${container.children[i].id}`,
-        start: 'top top', // 언제 어딜 지나면 시작할지
-        end: 'bottom top', 
-        ariaLabel: `#${container.children[i].id}`,
-        markers: true,
-        scrub: true, //바로바뀜이 아니라 지정만큼 천천히
-      }});
+            trigger: `#${container.children[i].id}`,
+            start: 'top top', // 언제 어딜 지나면 시작할지
+            end: 'bottom top', 
+            ariaLabel: `#${container.children[i].id}`,
+            // markers: true,
+            scrub: true, //바로바뀜이 아니라 지정만큼 천천히
+          },
+        });
       const plus = (i + 1) % container.children.length ;
       if(i === (container.children.length - 1)){
         return ; // 마지막 인자로 끝나도록
@@ -390,6 +223,30 @@ export default async function init () {
     }
   }
 
+  const reset = (plantArry)=> {
+    document.querySelector('#warning-click').removeAttribute('class','show');
+    plantArry.forEach( arr=>{ arr.animation('small'); arr.isClick = false;}) //animation
+  }
+
+  function handlerClickDouble (event , plantArry) {  
+    const intersects = raycatster.intersectObjects(scene.children);
+
+    if(intersects.length > 0  && !clickAni.isActive()){
+      const object = intersects[0].object;
+        if(object.name === 'skeletone' || object.name === 'cube'){
+         
+          selectArr[0].isDoubleClick = true;
+          console.log("dbclick");
+        }
+    }else{
+
+    }
+  }
+ 
+  function handlerWheel (event , plantArry) {
+    reset(plantArry);
+  } 
+
   const addEvent = (obj) => {
     const {  planetGroup, plantArry  } = obj;   
     window.addEventListener('resize', resize);
@@ -397,15 +254,31 @@ export default async function init () {
 
     window.addEventListener('pointermove',(event) =>  handlerPointerMove(event, plantArry));
     window.addEventListener('pointerdown',(event) =>  handlerPointerDown(event, plantArry));
-    
-   
+    window.addEventListener('dblclick', (event) => handlerClickDouble(event, plantArry));
+    window.addEventListener('wheel', (event) => handlerWheel(event, plantArry));
+
+    const home = document.querySelector('#home');
+    home.addEventListener('click', (event) =>{
+      document.querySelector("#planet-0").scrollIntoView({
+        behavior: 'smooth'
+      })
+    });
+
+    document.querySelectorAll('#right-buttons .button-in-header').forEach((element)=>{
+      element.addEventListener("click", (event) => {
+        document.getElementById("active-menu").removeAttribute("id");
+
+        element.setAttribute("id", "active-menu");
+      })
+    })
+
   };
+ 
 
   const draw = (obj) => {
     const {  planetGroup, plantArry  } = obj;   
     // earth.update(controls, clock.getElapsedTime());
     plantArry.forEach(planet => planet.update(clock.getElapsedTime()))
-    // camera.lookAt(planetGroup.children[5].position);
     controls.update();
     renderer.render(scene, camera);   
     requestAnimationFrame(() => {
@@ -415,7 +288,7 @@ export default async function init () {
 
   const initialize = async() => {
     const gui = new GUI();
-    
+    gui.hide();
     // container.appendChild(renderer.domElement);
     const obj = create(gui);
     addEvent(obj);
@@ -423,49 +296,6 @@ export default async function init () {
     resize();
     draw(obj);
 
-    // obj.planetGroup.rotation.set(4.8, 2.73, 0.37 );
-   
-
-   
-    const planetFold = gui.addFolder('objPosition').close();
-    planetFold.add(obj.planetGroup.rotation, 'x')
-    .min(-10)
-    .max(10)
-    .step(0.001);
-
-    planetFold
-    .add(obj.planetGroup.rotation, 'y')
-    .min(-10)
-    .max(10)
-    .step(0.001);
-
-
-    planetFold
-    .add(obj.planetGroup.rotation, 'z')
-    .min(-10)
-    .max(10)
-    .step(0.001);
-
-
-
-    const imgFold = gui.addFolder('imgPosition').close();
-    imgFold.add(obj.planetGroup.children[1].rotation, 'x')
-    .min(-10)
-    .max(10)
-    .step(0.001);
-
-    imgFold
-    .add(obj.planetGroup.children[1].rotation, 'y')
-    .min(-10)
-    .max(10)
-    .step(0.001);
-
-
-    imgFold
-    .add(obj.planetGroup.children[1].rotation, 'z')
-    .min(-10)
-    .max(10)
-    .step(0.001);
 
   };
 
--- src/js/models/Planet.js
+++ src/js/models/Planet.js
@@ -1,13 +1,18 @@
 import * as THREE from 'three';
 import gsap from 'gsap';
+import {TextGeometry} from 'three/examples/jsm/geometries/TextGeometry';
 
 export default class Planet {
-  constructor({cubeR, skeleR, texture, index, planetRadius: planetRadius} ){
+  constructor({cubeR, skeleR, texture, index, planetRadius: planetRadius, font: font, data: data} ){
     this.radius = cubeR;
     this.texture = texture;
     this.index = index / 60 - 1;
     this.radian = index * (Math.PI / 180);
     this.planetRadius = planetRadius;
+    this.font = font;
+    this.data = data;
+    this.isClick = false;
+
     // this.texture.magFilter = THREE.LinearMipmapLinearFilter;
     const createCube = ()=>{
       const cubeGeometry = new THREE.IcosahedronGeometry(cubeR);
@@ -49,20 +54,58 @@ export default class Planet {
       return mesh;
     }
 
+    const createText = (content) => {
+      const textGeometry = new TextGeometry(content,{ 
+        font : font,
+        size: 0.5,
+        height: 0.1,
+        bevelEnabled: true,
+        bevelSegments: 5,
+        bevelSize: 0.01,
+        bevelThickness: 0.02
+      });
+      textGeometry.computeBoundingBox(); //boundingbox의 값 계산하는 함수, 그 전엔 값 안나옴
+      textGeometry.center();
+      const textMeterial = new THREE.MeshLambertMaterial({
+        color: 0x33ffdd,
+        emissive: 0xccff77,
+      });
+      const text = new THREE.Mesh(textGeometry, textMeterial);
+      return text;
+    }
+
     this.cube = createCube();
     this.skeletone = createSkeletone();
 
     this.picture = createPlane();
     this.picture.visible = false;
     this.picture.rotation.set(-4.5, -0.3, 2.8);
+    
+    let showIndex = this.index - 3;
+    if(showIndex < 0){
+      showIndex += this.data.data.length;
+    }
+    this.showIndex = showIndex + 1;
+    this.numberText = createText((this.showIndex).toString());
+    this.titleText = createText(this.data.data[this.index].title);
+    this.titleText.scale.set(0.2, 0.2, 0.2);
+
+    // this.numberText.scale.set(4, 4, 4);
+    this.titleText.rotation.set(2.081, -0.655, 2.928);
+    this.numberText.rotation.set(1.642, -1.308, -3.274);
+
+    this.numberText.visible = false;
+    this.titleText.visible = false;
 
     this.planet = new THREE.Group();
     this.planet.userData = {
       dataIndex : this.index,
     }
 
-    this.planet.add(this.cube, this.skeletone, this.picture);
+    this.planet.add(this.cube, this.skeletone, this.picture, this.numberText, this.titleText );
     this.planet.position.set( planetRadius * Math.cos( this.radian ), planetRadius * Math.sin( this.radian ), 0);
+    // this.titleText.position.set( -0.049, -0.325, 0.205);
+    // this.numberText.position.set(0.871 ,-0.244 ,-0.767 );
     const hover = gsap.timeline({
       id: `${this.index}`,
       // smoothChildTiming: true,
@@ -70,6 +113,8 @@ export default class Planet {
     });
     this.hover = hover;
   }
+
+  
   animation(to){
     if(this.hover.isActive()) {
       this.hover.clear(true);
@@ -92,6 +137,39 @@ export default class Planet {
         opacity : 0.4,
         duration: 0.4,
       }, '<')
+      .to(this.numberText.scale, {
+        x : 1,
+        y : 1,
+        z : 1,
+        duration: 0.3,
+
+      }, '<')
+      .to(this.numberText.position, {
+        x : 0.871,
+        y : -0.244,
+        z : -0.767,
+        duration: 0.3,
+
+      }, '<')
+      .to(this.titleText.scale, {
+        x : 0.2,
+        y : 0.2,
+        z : 0.2,
+        duration: 0.3,
+
+      }, '<')
+      .to(this.titleText.position, {
+        x : -0.049,
+        y : -0.325,
+        z : 0.205,
+        duration: 0.3,
+        onStart: ()=>{
+          // if(document.querySelector(`#planet-${this.showIndex-1}`).scr()){
+            this.numberText.visible = true;
+            this.titleText.visible = true;
+          // }
+        },
+      }, '<')
       .to(this.picture.scale, {
         x : 2.3,
         y : 2.3,
@@ -101,7 +179,6 @@ export default class Planet {
           this.picture.visible = true;
         },
         onComplete: ()=>{
-       
           this.hover.clear(true);
         },
       }, '<');
@@ -123,6 +200,38 @@ export default class Planet {
         opacity : 1,
         duration: 0.4,
       }, '<')
+      .to(this.numberText.scale, {
+        x : 0,
+        y : 0,
+        z : 0,
+        duration: 0.3,
+
+      }, '<')
+      .to(this.numberText.position, {
+        x : this.planet.position.x,
+        y : this.planet.position.y,
+        z : this.planet.position.z,
+        duration: 0.3,
+
+      }, '<')
+      .to(this.titleText.scale, {
+        x : 0,
+        y : 0,
+        z : 0,
+        duration: 0.3,
+
+      }, '<')
+      .to(this.titleText.position, {
+        x : this.planet.position.x,
+        y : this.planet.position.y,
+        z : this.planet.position.z,
+        duration: 0.3,
+
+        onStart: ()=>{
+          this.numberText.visible = false;
+          this.titleText.visible = false;
+        },
+      }, '<')
       .to(this.picture.scale, {
         x : 1,
         y : 1,
@@ -130,6 +239,8 @@ export default class Planet {
         duration: 0.3,
         onComplete: ()=>{
           this.picture.visible = false;
+          // this.numberText.visible = false;
+          // this.titleText.visible = false;
           this.hover.clear(true);
         },
       }, '<');

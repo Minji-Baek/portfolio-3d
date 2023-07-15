6d5ea20a58be011e4a5d13b684ae54b2eebc168c
minjiBaek <emilyback@naver.com>
2023-07-15 15:59:52 +0900

    build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import vitePluginGLSL from 'vite-plugin-glsl';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vitePluginGLSL()],
-  base: '/portfolio-3d/'
+  base: '/portfolio-3d/',
+  // alias: {
+  //   "~": path.resolve(__dirname, "node_modules"),
+  //   "@": path.resolve(__dirname, "src"),
+  // },
+  build: {
+    chunkSizeWarningLimit: 1600,
+    },
 })

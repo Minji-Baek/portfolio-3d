0abb05fbbbd3b78f5c0c8e8228aedb7bcfba715e
minjiBaek <emilyback@naver.com>
2023-07-19 00:13:15 +0900

    vite config 변경

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
-import vitePluginGLSL from 'vite-plugin-glsl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // plugins: [vitePluginGLSL()],
-  root: 'src',
+  // root: 'src',
   base: '/portfolio-3d/',
   build: {
     chunkSizeWarningLimit: 1600,
